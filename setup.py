b1ea12804504ac0924c5f3de8358c5b7c9f812b8
hsienwei <hsienwei@gmail.com>
2015-06-29 00:46:14 +0800

    處理找不到module問題

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=["file_encode_converter.py"])
+setup(windows=["file_encode_converter.py"] ,
+      options = { "py2exe": { "includes": "chardet, Tkinter" } })
